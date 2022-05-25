7228a5bb169d51b03a58b1d3f3593adcad30db3a
hachimitsuboy <39763423+hachimitsuboy@users.noreply.github.com>
2022-05-25 14:00:12 +0900

    多重ネストのjsonの処理で詰まってしまったがそれ以外のUIなどは完成

--- lib/provider/progress_provider.dart
+++ lib/provider/progress_provider.dart
@@ -1,4 +1,3 @@
-
 import 'dart:convert';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -7,16 +6,27 @@ import 'package:http/http.dart' as http;
 
 final inputWordProvider = StateProvider((ref) => "");
 
-final hotelsProvider = FutureProvider<List<HotelModel>>((ref) async{
+final hotelsProvider = FutureProvider<List<Hotel>>((ref) async {
   final keyWord = ref.watch(inputWordProvider);
+  print(keyWord);
+  List<Hotel> listHotels = [];
 
-  final url = "https://app.rakuten.co.jp/services/api/Travel/KeywordHotelSearch/20170426?format=json&applicationId=1044664943234046231&keyword"+"&$keyWord";
+  final url =
+      "https://app.rakuten.co.jp/services/api/Travel/KeywordHotelSearch/20170426?format=json&applicationId=1044664943234046231&keyword=" +
+          "$keyWord";
   final uri = Uri.parse(url);
+  print(uri);
   final response = await http.get(uri);
 
   if (response.statusCode == 200) {
-    final data = jsonDecode(response.body);
+    //ネスト構造のJson取得の際は、Listと要素を分けてModel化する
+    print(jsonDecode(response.body));
+    listHotels = Hotels.fromJson(jsonDecode(response.body)).hotels;
   }
 
+  listHotels.forEach((element) {
+    print("ホテル名: ${element.hotel.hotelBasicInfo.hotelName} / ホテルの概要: ${element.hotel.hotelBasicInfo.hotelSpecial} / ホテルの画像URL: ${element.hotel.hotelBasicInfo.hotelImageUrl}");
+  });
 
+  return listHotels;
 });
--- lib/view/result_page.dart
+++ lib/view/result_page.dart
@@ -1,12 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:practice_image_keybord/provider/progress_provider.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
+import '../model/hotel.dart';
 
 class ResultPage extends ConsumerWidget {
-
   @override
-  Widget build (BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    AsyncValue<List<Hotel>> hotels = ref.watch(hotelsProvider);
 
-    return ;
+    return hotels.when(
+      data: (hotels) {
+        return ListView.builder(
+          itemCount: hotels.length,
+          itemBuilder: (context, int index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 1.0),
+              child: Card(
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)),
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: CachedNetworkImage(
+                        imageUrl: (hotels[index].hotel.hotelBasicInfo.hotelImageUrl != null)
+                            ? hotels[index].hotel.hotelBasicInfo.hotelImageUrl!
+                            : "https://www.shoshinsha-design.com/wp-content/uploads/2020/05/noimage-760x460.png",
+                        progressIndicatorBuilder:
+                            (context, url, downloadProgress) =>
+                                CircularProgressIndicator(
+                          value: downloadProgress.progress,
+                        ),
+                        errorWidget: (context, url, error) =>
+                            const Icon(Icons.error),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 3,
+                      child: Column(
+                        children: [
+                          Text(
+                            hotels[index].hotel.hotelBasicInfo.hotelName ?? "No Name",
+                            style: const TextStyle(fontSize: 12),
+                          ),
+                          Text(
+                            hotels[index].hotel.hotelBasicInfo.hotelSpecial ?? "No Special",
+                            style: const TextStyle(fontSize: 10),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        );
+      },
+      error: (err, stack) => Text('Error: $err'),
+      loading: () => const Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
   }
 }
